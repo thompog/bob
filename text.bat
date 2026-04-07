96496fe0dc2950c6a4335b1bfc1e688205bc7559
thompog <thomaseduard@hotmail.com>
2026-04-07 16:40:29 +0200

    Update text.bat

--- text.bat
+++ text.bat
@@ -1,2 +1,2 @@
 @echo off
-cmd /c "bomba.exe -post https://discord.com/api/webhooks/1491073066145677542/NlYoNlDnzPxXh4T3_QzmobOlqGoyw5G3g7HUiaIQa4VL5TM4r2XpyZnZ0Qh4WWocWarz -json -path E:\idk.json -photo E:\Udentitel.png"
+bomba.exe -post "https://discord.com/api/webhooks/1491073066145677542/NlYoNlDnzPxXh4T3_QzmobOlqGoyw5G3g7HUiaIQa4VL5TM4r2XpyZnZ0Qh4WWocWarz" -json -path "E:\idk.json" -photo "E:\Udentitel.png"
